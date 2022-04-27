5eea3ee5ac6667b4ff512fa0c79d885ee4e00069
Manuel Silva <mansilvar11@gmail.com>
2022-04-27 03:49:00 -0300

    Primer borrador pagina de vp

---
+++ pag/main.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '{Programador Web}',
+         '{Programador Front-End}'],
+         typeSpeed: 75,
+         startDelay: 300,
+         backSpeed: 120,
+         smartBackspace: true,
+         shuffle: false, 
+         backDelay: 1500,
+         loop: true,
+         loopCount: false,
+         showCursor: true,
+         cursorChar: '|',
+         contentType: 'html', 
+});
